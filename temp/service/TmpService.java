c0fde3969c157a965d34d005ffa0429303ddea3b
xiehanbing <xiebbing@126.com>
2018-02-08 12:55:57 +0800

    add servicepackage

---
+++ temp/service/TmpService.java
@@ -0,0 +1,11 @@
+package service;
+
+public class TmpService implements  ITmpService {
+
+	@Override
+	public String message() {
+		// TODO Auto-generated method stub
+		return "hello world";
+	}
+
+}
--- temp/singleProject/temp1.java
+++ temp/singleProject/temp1.java
@@ -1,10 +1,16 @@
 package singleProject;
+import service.*;
+import sun.management.counter.Variability;
 
 public class temp1 {
 
+	public static ITmpService _tmpService=new TmpService();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+System.out.println("nice boy");
+String mString=_tmpService.message();
+System.out.println(mString);
 	}
 
 }
