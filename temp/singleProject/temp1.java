d647a3175e6e1cebb63a16b7cefba536b331e85b
xiehanbing <xiebbing@126.com>
2018-02-08 16:34:19 +0800

    测试

--- temp/singleProject/temp1.java
+++ temp/singleProject/temp1.java
@@ -4,14 +4,21 @@ import sun.management.counter.Variability;
 
 public class temp1 {
 
+	/*接口定义*/
 	public static ITmpService _tmpService=new TmpService();
 	public static void main(String[] args) {
+		pri();
 		// TODO Auto-generated method stub
-System.out.println("hello world");
-System.out.println("nice boy");
-String mString=_tmpService.message();
-System.out.println(mString);
-System.out.println(_tmpService.isSuccess(true));
+//System.out.println("hello world");
+//System.out.println("nice boy");
+//String mString=_tmpService.message();
+//System.out.println(mString);
+//System.out.println(_tmpService.isSuccess(true));
 	}
 
+	private static void pri() {
+		// TODO Auto-generated method stub
+
+		System.out.println("基本类型：byte 二进制位数:"+Byte.SIZE);
+	}
 }
